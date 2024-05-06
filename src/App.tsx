c9ec478fcb61cc09d701f665c3993ce017c414bf
KasiaH <kah.gda@wp.pl>
2024-05-06 13:07:23 +0200

    created counter generator

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import { Counter } from './components/Counter/Counter';
 import { Generator } from './components/Generator/Generator';
 
 function App() {
     return (
         <>
             <Generator />
+            <Counter />
         </>
     );
 }
